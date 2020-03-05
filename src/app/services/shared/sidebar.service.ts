d8a2b455c21a7d5d7ff79835020fc900ce67dbe7
Estivenson Carvajal Salazar <ecarvajal@ingeneo.local>
2020-03-05 16:29:56 -0500

    =Sección 8: Observables y Promesas terminada

--- src/app/services/shared/sidebar.service.ts
+++ src/app/services/shared/sidebar.service.ts
@@ -12,7 +12,9 @@ export class SidebarService {
       subMenu: [
         { titulo: 'Dashboard', url: '/dashboard' },
         { titulo: 'ProgressBar', url: '/progress' },
-        { titulo: 'Gráficas', url: '/graficas1' }
+        { titulo: 'Gráficas', url: '/graficas1' },
+        { titulo: 'Promesas', url: '/promesas' },
+        { titulo: 'Rxjs', url: '/rxjs' }
       ]
     }
   ];
